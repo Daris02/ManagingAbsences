42880e6e5da4ca4d86702ec37953aaf0f386f8b2
Daris02 <hei.raymond.2@gmail.com>
2023-08-17 00:23:33 +0300

    docs: add examples for using mapping

--- Server/src/main/java/hei/server/controller/RegisterController.java
+++ Server/src/main/java/hei/server/controller/RegisterController.java
@@ -20,22 +20,31 @@ public class RegisterController {
 
     @GetMapping("/{type}")
     public List<Register> findAllRegisters(@PathVariable String type) {
+        // EXAMPLE PATH VARIABLE :
+        // -> {URL}/register/entry => TO get all row of entry
         return registerService.findAllRegisters(type);
     }
 
     @PostMapping("/{id}/{type}")
     public String addRegister(@PathVariable Integer id,@PathVariable String type) {
+        // EXAMPLE PATH VARIABLE :
+        //  ->  {URL}/register/1/entry TO add student with id=1 in entry register
+        //  ->  {URL}/register/2/exit  TO add student with id=2 in exit register
         registerService.addRegister(id, type);
         return "Add student to " + type + " successfully";
     }
 
     @GetMapping("/present")
     public List<Student> findAllStudentsPresent(@RequestBody String datetime) {
+        // TYPE OF REQUEST BODY IS "TEXT" :
+        //      Example : 2023-08-22 or 2023-08-22T08:00:00 or 2023-08-22 08:00:00
         return registerService.findAllStudentsPresent(datetime);
     }
 
     @GetMapping("/absent")
     public List<Student> findAllStudentsAbsent(@RequestBody String datetime) {
+        // TYPE OF REQUEST BODY IS "TEXT" :
+        //      Example : 2023-08-22 or 2023-08-22T08:00:00 or 2023-08-22 08:00:00
         return registerService.findAllStudentsAbsent(datetime);
     }
 }
