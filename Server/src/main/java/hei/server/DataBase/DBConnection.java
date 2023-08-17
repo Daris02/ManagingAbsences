c3a52b4dffb5bace0cf263efbaee9ce1c6b6a51e
Daris02 <hei.raymond.2@gmail.com>
2023-08-17 12:45:02 +0300

    fix: remove database authentication value

--- Server/src/main/java/hei/server/DataBase/DBConnection.java
+++ Server/src/main/java/hei/server/DataBase/DBConnection.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    private static final String databaseName = "jdbc:postgresql://localhost/" + "hei";
-    private static final String user = "postgres";
-    private static final String password = "postgres";
+    private static final String databaseName = System.getenv("DB_URL");
+    private static final String user = System.getenv("DB_USERNAME");
+    private static final String password = System.getenv("DB_PASSWORD");
     private static Connection connection;
 
     // Public Method to create connection
